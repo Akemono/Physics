a0d629ebc8d057a7200823094993817d2ae34123
Akemono <mikematrix@gmail.com>
2016-09-25 16:38:37 +0200

    Setup initiele les

---
+++ Assets/forward.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forward : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * 1 * Time.deltaTime);
+	}
+}
